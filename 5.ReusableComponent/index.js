9d794915fb5da89e86be07d93bf038bc08f08873
PuneetSivananda <psivananda@chatsoft.com>
2022-05-30 01:09:26 +0530

    chorE: updates

--- 5.ReusableComponent/index.js
+++ 5.ReusableComponent/index.js
@@ -3,18 +3,34 @@ import { LitElement, html } from 'lit';
 export class MyElement extends LitElement {
 	static get properties() {
 		return {
-			name: { type: Number }
+			step: { type: Number }
 		};
 	}
 
+	constructor() {
+		super();
+		this.step = 0;
+	}
+
 	onClick() {
-		console.log('focused');
+		const event = new CustomEvent('update-counter', {
+			bubbles: true,
+			detail: {
+				step: this.step + 1
+			}
+		});
+		console.log(event);
+		this.dispatchEvent(event);
 	}
 
 	render() {
+		const label =
+			this.step < 0
+				? `- ${-1 * this.step}` // "- 1"
+				: `+ ${this.step}`; // "+ 1"
 		return html`
       <button @click=${this.onClick}>
-        Click to Increment the count
+        Click to Increment the count ${label}
       </button>
    `;
 	}
