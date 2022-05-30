e61e279be9de474ccd9b5d32fd43a978564b7122
PuneetSivananda <psivananda@chatsoft.com>
2022-05-30 21:31:41 +0530

    cssmap and styling in lir

--- 6.styling/index.js
+++ 6.styling/index.js
@@ -1,5 +1,7 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { styleMap } from 'lit/directives/style-map.js';
+
+const ORANGE = css`orange`;
 
 class MyElement extends LitElement {
 	static get properties() {
@@ -7,7 +9,19 @@ class MyElement extends LitElement {
 			color: { type: String }
 		};
 	}
-	color = '#F53';
+	color = '#000';
+
+	static styles = [
+		css`
+			#orange {
+				color: ${ORANGE};
+			}
+
+			#purple {
+				color: rebeccapurple;
+			}
+		`
+	];
 
 	render() {
 		const headerStyle = styleMap({
@@ -26,6 +40,8 @@ class MyElement extends LitElement {
       type="color"
       @input=${(e) => (this.color = e.target.value)}
       value="#000">
+      <h3 id="orange">This text is in orange</h3>
+      <h3 id="purple">This text is in rebeccapurple</h3>
   </div>
     `;
 	}
